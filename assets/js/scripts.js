cdfd03e823143a48aa523d9c4a509fcff0c41f5f
Jason-LC-Studio <jason@lcstudio.co.za>
2020-10-17 00:41:36 +0200

    styling and extra setup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,3 +16,16 @@ let topFunction = () => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };
+
+// Animate logo
+var svg = document.getElementsByTagName("svg")[0];
+var bbox = svg.getBBox();
+var viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ");
+svg.setAttribute("viewBox", viewBox);
+
+let AnimateLogo = () => {
+  document.getElementById("Logo").setAttribute("class", "animated-logo");
+  setTimeout(function () {
+    document.getElementById("Logo").setAttribute("class", "static-logo");
+  }, 2200);
+};
